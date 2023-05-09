1d7328ddbf35df14f3fe5b4aa3ce5f48da71d748
ebransita <ebranista@19gmail.com>
2023-05-09 10:54:43 -0700

    updated

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for c in range(97, 123):
+    if c != 101 and c != 123:
+        print('{}'.format(chr(c)), end='')
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for in range(99):
+    print("{}".format(i, hex(i)))
