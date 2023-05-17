393029dfa64ce525e79e100e28a96585385ab397
ebranista <ebranista19@gmail.com>
2023-05-17 11:08:01 -0700

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return (None)
+    else:
+        a = list(a_dictionary.values())
+        b = list(a_dictionary.keys())
+        return (a[b.index(max(a))])
