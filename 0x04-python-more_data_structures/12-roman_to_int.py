6962eaa51bd1e6f6b0668ba04f695be1e7dcde53
ebranista <ebranista19@gmail.com>
2023-05-17 11:30:08 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def romant_int(roman_string):
+def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
         return (0)
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000)
