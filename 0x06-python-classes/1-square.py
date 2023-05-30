6badb606da7fb03275b80c8f2174d4b5c0d8cdd6
ebranista <ebranista19@gmail.com>
2023-05-30 10:10:57 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' Creates empty class called square
+'''
+
+cleass square:
+    def __init__(self, size):
+        ''' empty class with size private attribute
+        '''
+        def __init__(self, size):
+            '''
+                insitantiation with size
+
+            Args:
+                size: size of square
+            '''
+            self.__size = size
