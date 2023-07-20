c3c2c4d548a8e9293577925749c368ce0eb829ee
ebranista <ebranista19@gmail.com>
2023-07-20 13:36:58 -0700

    task update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
-mysql -u root -p root
+-- List all privileges for users 'user_0d_1' and 'user_0d_2'
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
